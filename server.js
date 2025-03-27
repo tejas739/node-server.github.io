95c6a00eed98f2a356c041844b211d4807f512e0
tejas739 <tejaswangdare123@gmail.com>
2025-03-27 18:28:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -49,4 +49,5 @@ app.get("/get-forms", (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on http://0.0.0.0:${PORT}`));
+
